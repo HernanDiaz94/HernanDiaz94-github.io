5c98c3a95b0de0d0aa6147073f302f5c9f815dc9
hernandiaz94 <hernan.diiaz@gmail.com>
2023-03-16 21:30:47 -0300

    cambios seccion contacto v1

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ function seleccionar() {
 }
 //Funcion que aplica las animaciones de las habilidades
 function efectoHabilidades() {
-  var skills = document.getElementById("skills");
-  var distancia_skills =
-    window.innerHeight - skills.getBoundingClientRect().top;
-  if (distancia_skills >= 300) {
+  var habilidades = document.getElementById("habilidades");
+  var distancia_habilidades =
+    window.innerHeight - habilidades.getBoundingClientRect().top;
+  if (distancia_habilidades >= 300) {
     let habilidades = document.getElementsByClassName("progreso");
     habilidades[0].classList.add("javascript");
     habilidades[1].classList.add("htmlcss");
